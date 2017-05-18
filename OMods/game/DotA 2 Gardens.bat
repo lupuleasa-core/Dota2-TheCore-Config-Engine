7e00d8044e6b3c9f587c8266ab007b47bb2be17d
Karrandalf <B.h.pepper@gmail.com>
2017-05-18 15:23:28 -0500

    Delete DotA 2 Gardens.bat

--- OMods/game/DotA 2 Gardens.bat
+++
@@ -1,9 +0,0 @@
-@RD /S /Q "mod_MapReplacer"
-if not exist "modding_Tools\VPK CREATOR\dota" mkdir "modding_Tools\VPK CREATOR\dota"
-"%~dp0\modding_Tools\hllib245\bin\x64\HLExtract.exe" -p "dota\maps\dota.vpk" -d "modding_Tools\VPK CREATOR\dota" -e "maps"
-"%~dp0\modding_Tools\hllib245\bin\x64\HLExtract.exe" -p "dota\maps\dota_coloseum.vpk" -d "modding_Tools\VPK CREATOR\dota" -e "maps"
-"%~dp0\modding_Tools\VPK CREATOR\vpk.exe" "modding_Tools\VPK CREATOR\dota"
-@RD /S /Q "modding_Tools\VPK CREATOR\dota"
-if not exist "mod_MapReplacer\maps" mkdir "mod_MapReplacer\maps"
-move /Y "modding_Tools\VPK CREATOR\dota.vpk" "mod_MapReplacer\maps\dota.vpk"
-START steam://rungameid/570
